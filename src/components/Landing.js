3ea39d353ddd124be170be2fee5708895956026a
Mahdi Niyati <Niyat2612@gmail.com>
2023-03-04 17:16:39 +0330

    filter

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -5,10 +5,12 @@ import { getCoin } from '../services/api';
 
 //components
 import Loader from './Loader';
+import Coin from './Coin';
 
 const Landing = () => {
 
     const [coins, setCoins] = useState([]);
+    const [search, setsearch]= useState("");
 
     useEffect(() => {
         const fetchAPI = async () => {
@@ -20,15 +22,29 @@ const Landing = () => {
         fetchAPI();
     }, [])
 
-    return (
+    const serachHandler = event=> {
+        setsearch(event.target.value)
+    }
+
+    const searchedCoin = coins.filter(coin => coin.name.toLowerCase().includes(search.toLocaleLowerCase()))
+
+    return(
         <>
-            <input type="text" placeholder="Search" />
+            <input type="text" placeholder="Search" value={search} onChange={serachHandler}/>
             {
                 coins.length ? 
                         <div>
                             
                             {
-                                coins.map(coin => <p key={coin.id}>{coin.name}</p>)
+                                searchedCoin.map(coin => <Coin 
+                                    key={coin.id}
+                                    name={coin.name}
+                                    image={coin.image}
+                                    symbol={coin.symbol}
+                                    price={coin.current_price}
+                                    marketCap={coin.market_cap}
+                                    priceChange={coin.price_change_percentage_24h}
+                                />)
                             }
                         </div> :
                 <Loader />
