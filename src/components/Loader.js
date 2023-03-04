cd48547bd7e6852ddb5b314c99bdb759752ad965
Mahdi Niyati <Niyat2612@gmail.com>
2023-03-04 16:32:19 +0330

    API

---
+++ src/components/Loader.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+//git
+import spinner from "../gif/spinner.gif"
+
+
+const Loader = () => {
+    return (
+        <div>
+            <img src={spinner} alt="loading" />
+            <h1>Loading ...</h1>
+        </div>
+    );
+};
+
+export default Loader;
